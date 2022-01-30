b84f1a5ba10dc1bc4561a3b06c02fe70031f3582
seo-faper <dswhdcks@gmail.com>
2022-01-30 15:56:43 +0900

    정수 삼각형 DP

--- testCode.py
+++ testCode.py
@@ -1,41 +1,4 @@
-# SET : 
-'''
-    [0,10,1,100,10]
-    [1,2] = 1
-    [1,3] = 100
-    [2,4] = 10
-    [3,4] = 10
+l = [1,2,3]
 
-'''
-import sys
-INF = sys.maxsize
-def Floyd_Warshall():
-    dist = [[INF]*n for i in range(n)]
-
-    for i in range(n):
-        for j in range(n):
-            dist[i][j] = G[i][j]
-
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if dist[i][j] > dist[i][k] + dist[k][j]:
-                    dist[i][j] = dist[i][k] + dist[k][j] 
-    return dist
-
-n = 5
-G = [[INF for _ in range(n)] for _ in range(n)]
-for i in range(1,n):
-    G[i][i] = 0
-
-G[1][2] = 1
-G[1][3] = 100
-G[2][3] = 100
-G[3][4] = 10
-
-dist = Floyd_Warshall()
-
-for i in range(n):
-    for j in range(n):
-        print(dist[i][j],end=' ')
-    print()
+for i in range(2,len(l)):
+    print(l[i])
