c719510142a708d0b893d8464e9460c8d2fa00b4
seo-faper <dswhdcks@gmail.com>
2022-05-18 19:12:36 +0900

    set

---
+++ 11478.py
@@ -0,0 +1,14 @@
+
+S = input()
+
+
+S1 = set()
+
+for i in range(0,len(S)):
+    for j in range(0,len(S)):
+        if i+j < len(S):
+
+            S1.add(S[j:i+j+1])
+
+
+print(len(S1))
--- testCode.py
+++ testCode.py
@@ -1,5 +1,2 @@
-import itertools
-
-N,M = map(int, input().split())
-
-print(len(list(itertools.combinations(range(N), M))))
+S = "abcde"
+print(S[0:5])
