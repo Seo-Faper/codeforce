a22505307598aeff8b8bede552daf546f0d72cec
seo-faper <dswhdcks@gmail.com>
2022-06-20 17:33:31 +0900

    우선순위 큐

--- 23757.py
+++ 23757.py
@@ -1,9 +1,15 @@
+import heapq
+heap = []
+N,M = map(int,input().split())
+L = list(map(int,input().split()))
+R = list(map(int,input().split()))
 
-import sys
-
-
-n,m = map(int, sys.stdin.readline().split())
-
-L1 = list(map(int, sys.stdin.readline().split()))
-L2 = list(map(int, sys.stdin.readline().split()))
-
+for i in L:
+    heapq.heappush(heap, -1*i)
+    
+ans = 1
+for i in R:
+    P = (-1*heapq.heappop(heap))
+    if P < i: ans = 0
+    else: heapq.heappush(heap, -1*(P-i))
+print(ans)
