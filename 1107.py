8453d0f936f34cf2fe25f10324d6936a8eff7df0
seo-faper <dswhdcks@gmail.com>
2022-09-06 23:34:40 +0900

    구현 문제, 브루트포스

--- 1107.py
+++ 1107.py
@@ -1,4 +1,15 @@
-N = int(input())
-input()
-L = list(map(int,input().split(" ")))
+import sys
+input = sys.stdin.readline
+target = int(input())
+n = int(input())
+b = list(map(int, input().split()))
+ans = abs(100 - target)
 
+for i in range(1000001):
+    i = str(i)
+    for j in range(len(i)):
+        if int(i[j]) in b: break
+        elif j == len(i)-1:
+            ans = min(ans,abs(int(i) - target)+len(i))
+
+print(ans)
