701a9ff4244bdc491be3f0dc82b7b85c12f290e4
Seo-Faper <dswhdcks@gmail.com>
2024-01-07 20:38:18 +0900

    백트래킹 연습

---
+++ js/Baekjoon/15650.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number)
+
+N = input[0]
+M = input[1]
+let arr = [];
+const recur = ((number)=>{
+    if(number === M){
+        console.log(arr.toString().replaceAll(',',' '));
+        return
+    }
+    for(let i = 1; i<=N; i++){
+        if(arr.indexOf(i) !== -1 || arr.at(-1) > i) continue
+        arr.push(i)
+        recur(number+1)
+        arr.pop()
+    }
+})
+
+recur(0)
