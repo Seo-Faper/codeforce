a7f4a1610afc51b6a7e1fcf47d1e91bc1f3b9c16
Seo-Faper <dswhdcks@gmail.com>
2024-02-25 22:48:39 +0900

    .,

--- python/4158.py
+++ python/4158.py
@@ -1,18 +1,5 @@
 import sys
 
-def binary_search(target, data):
-    start = 0
-    end = len(data) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if data[mid] > target:
-            end = mid - 1
-        elif data[mid] < target:
-            start = mid + 1
-        else:
-            return True
-    return False
 input = sys.stdin.readline
 while True:
     n,m = map(int, input().split())
@@ -22,8 +9,6 @@ while True:
     else:
         array = set([int(input()) for _ in range(n)])
         cd_no = set([int(input()) for _ in range(m)])
-    
-        for i in cd_no:
-            if binary_search(i,array):
-                ans+=1
+        for i in array:
+            if i in cd_no: ans+=1
     print(ans)
