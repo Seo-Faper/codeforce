9c74b14e8a7cfd99f4f7add6ea6b9da9cba89615
Seo-Faper <dswhdcks@gmail.com>
2025-05-01 20:41:35 +0900

    유니코드드

---
+++ python/12090.py
@@ -0,0 +1,5 @@
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+s = input()
+for i in s:
+    print(CHOSUNG_LIST[(ord(i) - ord('가'))//588],end='')
+print()
